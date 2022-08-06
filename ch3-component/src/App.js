3769b9d3fec5e3d97ab0874761bbcaf196ebcf49
최원영 <sable@kangwon.ac.kr>
2022-08-06 11:08:26 +0000

    a

--- ch3-component/src/App.js
+++ ch3-component/src/App.js
@@ -1,6 +1,6 @@
 import MyComponent from './MyComponent'
 
 const App =() => {
-	return <MyComponent name = "React" />;
+	return <MyComponent name = "React" a = "Aprops" > // 마이컴포넌트 (mycomponent 태그 사이) //</MyComponent>;
 };
 export default App;
--- ch3-component/src/MyComponent.js
+++ ch3-component/src/MyComponent.js
@@ -1,9 +1,12 @@
 const MyComponent = (props) => {
+	const {a, children } = props; // 편함
 	return (
 		<>
 			<div>나의 새로운 컴포넌트 ! MyComponent</div>
 			<div>안녕하세요! 제 이름은 {props.name}입니다. </div>
 			<div>안녕하세요! 제 디폴트 이름은 {props.name2}입니다. </div>
+			<div>children 값 =>  {children}</div>
+			<div>{a}</div>
 				
 		</>
 		);
