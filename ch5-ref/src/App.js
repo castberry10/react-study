05f199c0b762a7baf1288b8b30294ee1f81b0d3c
최원영 <sable@kangwon.ac.kr>
2022-08-09 12:19:46 +0000

    수요일이 아니라 화요일

--- ch5-ref/src/App.js
+++ ch5-ref/src/App.js
@@ -1,11 +1,16 @@
 import {Component} from 'react';
 import ValidationSample from './ValidationSample';
+import ScrollBox from './ScrollBox'
 
 class App extends Component{
 	render(){
 		return(
 			<div>
 				<ValidationSample/>
+				<br/>
+				---------------------------------
+				<br/>
+				<ScrollBox/>
 			</div>
 		)
 	}
