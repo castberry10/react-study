e0f681da8a04618e4fd041915c1add2f2bbf9fcc
최원영 <sable@kangwon.ac.kr>
2023-07-04 08:28:13 +0000

    context 반영 1

--- context-tutorial/src/components/ColorBox.js
+++ context-tutorial/src/components/ColorBox.js
@@ -1,19 +1,28 @@
-import ColorContext from "../contexts/color";
+import {ColorConsumer} from '../contexts/color';
 
 
 const ColorBox = () => {
     return(
-        <ColorContext.Consumer>
+        <ColorConsumer>
             {value => (
+				<div>
                 <div 
                     style={{
                         width: '64px',
                         height: '64px',
-                        background: value.color
+                        background: value.state.color
                     }}
                 />
+				<div
+					style={{
+							width: '32px',
+							height: '32px',
+							background: value.state.subcolor
+						}}/>
+				</div>
             )}
-        </ColorContext.Consumer>
+
+        </ColorConsumer>
     );
 };
 
