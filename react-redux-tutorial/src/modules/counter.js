427cdcc4cb77330acb25b0ccf498ec19ca7e2fa3
최원영 <sable@kangwon.ac.kr>
2023-07-08 08:10:26 +0000

    카운터 모듈 생성

--- react-redux-tutorial/src/modules/counter.js
+++ react-redux-tutorial/src/modules/counter.js
@@ -7,3 +7,24 @@ const DECREASE = 'counter/DECREASE';
 
 export const increase = () => ({type : INCREASE});
 export const decerase = () => ({type : DECREASE});
+
+// 초기상태
+const initalState = {
+    number: 0
+};
+
+function counter(state = initalState, action) {
+    switch(action.type){
+        case INCREASE:
+            return{
+                number: state.number + 1
+            };
+        case DECREASE:
+            return{
+                number: state.number + 1
+            };
+        default:
+            return state;    
+    }
+}
+export default counter;
