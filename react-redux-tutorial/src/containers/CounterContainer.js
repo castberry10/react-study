b4933cdea071d1d278c52b336ba83af207c214c7
최원영 <sable@kangwon.ac.kr>
2023-07-09 10:50:45 +0000

    bindActionCreators 사용

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,43 +1,45 @@
 import Counter from "../components/Counter";
 import {connect} from "react-redux"
 import {increase, decrease} from '../modules/counter';
+import { bindActionCreators } from "redux";
 
 const CounterContainer = ({number, increase, decrease}) => {
     return (<Counter number = {number} onIncrease={increase} onDecrease={decrease} />);
 };
 
-
-const mapStateToProps = state => ({
-    number: state.counter.number,
-
-});
-
-const mapDispatchToProps = dispatch => ({
-    // temp function 
-    increase : () => {
-        // console.log('increase');
-        dispatch(increase());
-    },
-    decrease: () => {
-        // console.log('decrease');
-        dispatch(decrease());
-    },
-});
-
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps,
+    state => ({
+        number: state.counter.number,
+    }),
+    dispatch => 
+        bindActionCreators(
+            {
+                increase,
+                decrease,   
+            },
+        dispatch,
+    ),
 )(CounterContainer);
-
 
 /* 기호에 따라 아래와 같이 작성해도 됨 */
 
+// const mapStateToProps = state => ({
+//     number: state.counter.number,
+
+// });
+// const mapDispatchToProps = dispatch => ({
+//     // temp function 
+//     increase : () => {
+//         // console.log('increase');
+//         dispatch(increase());
+//     },
+//     decrease: () => {
+//         // console.log('decrease');
+//         dispatch(decrease());
+//     },
+// });
+
 // export default connect(
-//     state => ({
-//         number: state.counter.number,
-//     }),
-//     dispath => ({
-//         increase: () => dispatch(increase()),
-//         decrease: () => dispatch(decrease()),
-//     }),
+//     mapStateToProps,
+//     mapDispatchToProps,
 // )(CounterContainer);
