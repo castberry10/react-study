0a1351474fc7f6e970f774db60ccf34b405c8180
최원영 <sable@kangwon.ac.kr>
2023-07-13 11:20:20 +0000

    counter 모듈에 redux-actions 적용

--- react-redux-tutorial/src/modules/counter.js
+++ react-redux-tutorial/src/modules/counter.js
@@ -1,3 +1,5 @@
+import {createAction, handleActions} from 'redux-actions'
+
 // 액션타입은 대문자로 정의
 // 모듈이름/액션이름
 // 프로젝트가 커졌을때 액션 충돌 방지
@@ -5,26 +7,36 @@ const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
 
 
-export const increase = () => ({type : INCREASE});
-export const decrease = () => ({type : DECREASE});
+// export const increase = () => ({type : INCREASE});
+// export const decrease = () => ({type : DECREASE});
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 // 초기상태
 const initalState = {
     number: 0
 };
 
-function counter(state = initalState, action) {
-    switch(action.type){
-        case INCREASE:
-            return{
-                number: state.number + 1
-            };
-        case DECREASE:
-            return{
-                number: state.number - 1
-            };
-        default:
-            return state;    
-    }
-}
+// function counter(state = initalState, action) {
+//     switch(action.type){
+//         case INCREASE:
+//             return{
+//                 number: state.number + 1
+//             };
+//         case DECREASE:
+//             return{
+//                 number: state.number - 1
+//             };
+//         default:
+//             return state;    
+//     }
+// }
+
+const counter = handleActions({
+    [INCREASE]: (state, action) => ({number:state.number + 1}),
+    [DECREASE]: (state, action) => ({number:state.number - 1}),
+    },
+    initalState,
+);
+
 export default counter;
