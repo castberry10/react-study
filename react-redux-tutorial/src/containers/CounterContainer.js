3b39fff2180624e8b0d62bab8c04f068b1d01e12
최원영 <sable@kangwon.ac.kr>
2023-07-15 09:28:29 +0000

    useCallback을 이용한 useDispatch 최적화

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -2,7 +2,7 @@ import Counter from "../components/Counter";
 // import {connect} from "react-redux"
 import {increase, decrease} from '../modules/counter';
 import {useSelector, useDispatch} from 'react-redux';
-
+import {useCallback} from 'react';
 /* 
 useSelector을(를) 사용하면 connect 함수를 사용하지 않고 리덕스 상태 조회 가능
 useDispatch을(를) 사용하면 컴포넌트 내부에서 스토어의 dispatch를 사용 가능
@@ -10,7 +10,10 @@ useDispatch을(를) 사용하면 컴포넌트 내부에서 스토어의 dispatch
 const CounterContainer = () => {
 	const number = useSelector(state => state.counter.number);
 	const dispatch = useDispatch();
-	return <Counter number={number} onIncrease={() => dispatch(increase())} onDecrease = {() => dispatch(decrease())}/>;
+	const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+	const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
+	
+	return <Counter number={number} onIncrease={onIncrease} onDecrease = {onDecrease}/>;
 }
 
 export default CounterContainer;
