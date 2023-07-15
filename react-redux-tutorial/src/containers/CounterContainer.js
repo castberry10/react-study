56aa5b28b01f0101b9f6a43f15461c0f9d4e08e0
최원영 <sable@kangwon.ac.kr>
2023-07-15 09:43:03 +0000

    TodosContainer를 Hooks로 전환

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,5 +1,4 @@
 import Counter from "../components/Counter";
-// import {connect} from "react-redux"
 import {increase, decrease} from '../modules/counter';
 import {useSelector, useDispatch} from 'react-redux';
 import {useCallback} from 'react';
@@ -17,39 +16,3 @@ const CounterContainer = () => {
 }
 
 export default CounterContainer;
-// const CounterContainer = ({number, increase, decrease}) => {
-//     return (<Counter number = {number} onIncrease={increase} onDecrease={decrease} />);
-// };
-
-// export default connect(
-//     state => ({
-//         number: state.counter.number,
-//     }),
-//     {
-//         increase,
-//         decrease,
-//     },
-// )(CounterContainer);
-
-/* 기호에 따라 아래와 같이 작성해도 됨 */
-
-// const mapStateToProps = state => ({
-//     number: state.counter.number,
-
-// });
-// const mapDispatchToProps = dispatch => ({
-//     // temp function 
-//     increase : () => {
-//         // console.log('increase');
-//         dispatch(increase());
-//     },
-//     decrease: () => {
-//         // console.log('decrease');
-//         dispatch(decrease());
-//     },
-// });
-
-// export default connect(
-//     mapStateToProps,
-//     mapDispatchToProps,
-// )(CounterContainer);
