514792de4e186641e67eb7a7a4714683c5314c02
최원영 <sable@kangwon.ac.kr>
2023-07-22 11:03:48 +0000

    리덕스 미들웨어를 통한 비동기 작업 관리 / 시작

--- learn-redux-middleware/src/components/Counter.js
+++ learn-redux-middleware/src/components/Counter.js
@@ -3,7 +3,7 @@ const Counter = ({onIncrease, onDecrease, number }) => {
 		<div>
 			<h1>{number}</h1>
 			<button onClick={onIncrease}>+1</button>
-			<button onClick={onDecrease}>+1</button>
+			<button onClick={onDecrease}>-1</button>
 			
 		</div>
 	);
