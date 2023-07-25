e5f5f52f9e47dd51dfe1eedd54bc1a81546e5149
최원영 <sable@kangwon.ac.kr>
2023-07-25 11:29:33 +0000

    미들웨어 예제 1

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import rootReducer from './modules'
+import loggerMiddleware from './lib/loggerMiddleware'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
 
 root.render(
 	<Provider store={store}>
