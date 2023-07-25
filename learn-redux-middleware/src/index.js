0a0e8f92a079676aacf03935b12e72382bd2ef33
최원영 <sable@kangwon.ac.kr>
2023-07-25 11:33:48 +0000

    미들웨어 예제 1보다 진보된 redux-logger 예제

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -7,9 +7,10 @@ import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import rootReducer from './modules'
 import loggerMiddleware from './lib/loggerMiddleware'
-
+import {createLogger} from 'redux-logger';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 root.render(
 	<Provider store={store}>
