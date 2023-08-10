00cc98361cf4628a59af60b4231005ec251bf435
최원영 <sable@kangwon.ac.kr>
2023-08-10 10:48:33 +0000

    리팩토링 후 & 로딩 상태를 관리하는 리덕스 모듈 생성 및 적용

--- learn-redux-middleware/src/modules/sample.js
+++ learn-redux-middleware/src/modules/sample.js
@@ -1,6 +1,6 @@
 import {handleActions} from 'redux-actions';
 import * as api from '../lib/api';
-// import createRequestThunk from '../lib/createRequestThunk';
+import createRequestThunk from '../lib/createRequestThunk';
 // 액션타입 선언
 // 한요청당 세 개를 만들어야한다. 
 
@@ -15,41 +15,9 @@ const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 //thunk 함수 생성
 // 내부에서는 시작, 성공, 실패했을 때 다른 액션을 디스패치한다. 
 
-export const getPost = id => async dispatch => {
-	dispatch({type: GET_POST});
-	try{
-		const response = await api.getPost(id);
-		dispatch({
-			type: GET_POST_SUCCESS,
-			payload: response.data
-		}); //요청 성공
-	} catch(e){
-		dispatch({
-			type: GET_POST_FAILURE,
-			payload: e,
-			error: true
-		}); // 에러 발생
-		throw e; // 나중에 컴포넌트단에서 에러를 조회하기 위함
-	}
-};
+export const getPost = createRequestThunk(GET_POST, api.getPost);
+export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
 
-export const getUsers = () => async dispatch => {
-	dispatch({type: GET_USERS}); // 요청 시작을 알림
-	try{
-		const response = await api.getUsers();
-		dispatch({
-			type: GET_USERS_SUCCESS,
-			payload: response.data
-		}); //요청 성공
-	} catch(e){
-		dispatch({
-			type: GET_USERS_FAILURE,
-			payload: e,
-			error: true
-		}); // 에러 발생
-		throw e; // 나중에 컴포넌트단에서 에러를 조회하기 위함
-	}
-};
 
 // 초기 상태를 선언
 // 요청의 로딩 중 상태는 loading 객체에서 관리
