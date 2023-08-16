c1ea735e42fb64329728950988adadc8787d3e06
최원영 <sable@kangwon.ac.kr>
2023-08-16 12:14:15 +0000

    리덕스 개발자도구 적용

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -6,16 +6,18 @@ import reportWebVitals from './reportWebVitals';
 import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import rootReducer,{rootSaga} from './modules'
-import loggerMiddleware from './lib/loggerMiddleware'
+// import loggerMiddleware from './lib/loggerMiddleware'
 import {createLogger} from 'redux-logger';
 import ReduxThunk from 'redux-thunk';
 import createSagaMiddleware from 'redux-saga';
+import {composeWithDevTools} from 'redux-devtools-extension';
 
 const sagaMiddleware = createSagaMiddleware();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const logger = createLogger();
+//composeWithDevTools를 redux middleware와 함께 사용한다면 applyMiddleware를 감싸면된다. 
 const store = createStore(rootReducer, 
-						  applyMiddleware(logger, ReduxThunk, sagaMiddleware));
+						  composeWithDevTools(applyMiddleware(logger, ReduxThunk, sagaMiddleware)));
 
 sagaMiddleware.run(rootSaga);
 root.render(
