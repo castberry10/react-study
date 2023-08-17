82a840c0dd3d720a6d41f5cbaec3109a215ff0d3
최원영 <sable@kangwon.ac.kr>
2023-08-17 11:29:03 +0000

    redux-saga -> select / 현재상태조회

--- learn-redux-middleware/src/modules/counter.js
+++ learn-redux-middleware/src/modules/counter.js
@@ -1,9 +1,10 @@
 import { createAction, handleActions} from 'redux-actions';
-import {delay, put, takeEvery, takeLatest} from 'redux-saga/effects';
+import {delay, put, takeEvery, takeLatest, select} from 'redux-saga/effects';
 
 
 const INCREASE = 'counter/INCREASE'
 const DECREASE = 'counter/DECREASE'
+
 const INCREASE_ASYNC = 'counter/INCREASE_ASYNC'
 const DECREASE_ASYNC = 'counter/DECREASE_ASYNC'
 
@@ -16,6 +17,8 @@ export const decreaseAsync = createAction(DECREASE_ASYNC, () => undefined);
 function* increaseSaga(){
 	yield delay(1000);
 	yield put(increase()); // 액션 디스패치
+	const number = yield select(state => state => state.counter); // state는 스토어 상태를 의미
+	console.log(`현재 값은 ${number}입니다.`)
 }
 
 function* decreaseSaga(){
