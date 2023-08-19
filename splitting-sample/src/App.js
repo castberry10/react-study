d93a6c68325f0a81adfa3bbfc1b98ebbbd8c2586
최원영 <sable@kangwon.ac.kr>
2023-08-19 08:05:43 +0000

    import를 함수로 이용

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+// import notify from './notify';
 function App() {
+	const onClick = () => {
+		// notify();
+		import('./notify').then(result => result.default()); // 표준 문법은 아니다. import를 함수로 사용하면 Promise를 반환
+	};
+	
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p onClick={onClick}>
           Hello react!
         </p>
         
