562da00fc10f2be7792415368b001aa13c7da9e8
최원영 <sable@kangwon.ac.kr>
2023-08-21 14:00:56 +0000

    React.lazy와 Suspense 사용하기

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,34 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 import {Component} from 'react';
-// import notify from './notify's;
-// const SplitMe = React.lazy(()=> import ('./SplitMe'))
+import {useState, Suspense} from 'react';
+import React from 'react';
 
-class App extends Component {
-	state = {
-		SplitMe: null
+const SplitMe = React.lazy(()=> import ('./SplitMe'));
+
+function App(){
+	
+	const [visible, setVisible] = useState(false);
+	const onClick = () => {
+		setVisible(true);
 	};
 	
-	handleClick = async () => {
-		const loadedModule = await import('./SplitMe');
-		this.setState({
-			SplitMe: loadedModule.default
-			
-		});
-	};
-	
-	render() {
-		const {SplitMe} = this.state;
-		return(
+	return(
 		<div className="App">
 			<header className="App-header">
         	<img src={logo} className="App-logo" alt="logo" />
-        	<p onClick={this.handleClick}>Hello react!</p>
-			{SplitMe && <SplitMe/>}
+        	<p onClick={onClick}>Hello react!</p>
+			<Suspense fallback={<div>loading...</div>}>
+				{visible && <SplitMe/>}
+			</Suspense>
 			</header>
     	</div>
-		)
-  }
+	);
+  
 }
 
 export default App;
