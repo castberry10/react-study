422420916f371977551b9205c4c2b12165394d41
최원영 <sable@kangwon.ac.kr>
2023-10-01 08:12:09 +0000

    git set

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,7 +1,21 @@
+require('dotenv').config();
+
 const Koa = require('koa');
 const Router = require('koa-router');
 const api = require('./api');
 const bodyParser = require('koa-bodyparser');
+const mongoose = require('mongoose');
+
+const {PORT, MONGO_URI} = process.env;
+
+
+mongoose.connect(MONGO_URI)
+	.then(()=>{
+	console.log('Connected to MongoDB')
+	})
+	.catch(e => {
+	console.error(e);
+});
 
 const app = new Koa();
 const router = new Router();
@@ -15,7 +29,9 @@ app.use(bodyParser());
 // app 인스터스에 라우터 적용 
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-	console.log('Listening to port 4000');
+const port = PORT || 4000;
+
+app.listen(port, () => {
+	console.log('Listening to port %d', port);
 });
 
