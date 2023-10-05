bce158a3273dc2bcf0fdac79c34bf87002ffcdfa
최원영 <sable@kangwon.ac.kr>
2023-10-05 12:29:07 +0000

    사용자 정의 user 스키마 인스턴트 메서드 생성

--- blog/blog-backend/src/models/user.js
+++ blog/blog-backend/src/models/user.js
@@ -1,10 +1,24 @@
 const mongoose = require('mongoose'); // common js 
 const {Schema} = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const UserSchema = new Schema({
 	username: String,
 	hashedPassword: String,
 });
+
+// 사용자정의 인스턴트 메서드
+UserSchema.methods.setPassword = async function(password){
+	const hash = await bcrypt.hash(password, 10);
+	this.hashedPassword = hash;
+};
+// 사용자정의 인스턴트 메서드
+UserSchema.methods.checkPassword = async function(password){
+	const result = await bcrypt.compare(password, this.hashedPassword);
+	retrun result; // true / false
+};
+
+
 const User = mongoose.model('User', UserSchema);
 export default User;
 
