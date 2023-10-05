2daacdfc92492ea765aa2e0fe3143e9fb533f121
최원영 <sable@kangwon.ac.kr>
2023-10-05 11:01:09 +0000

    페이지 기능 구현 < skip((page-1) * 10)

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -46,8 +46,16 @@ exports.write = async ctx => {
 };
 
 exports.list = async ctx => {
+	//query는 문자열이어서 숫자로 변환
+	//default 값으로 1을 사용
+	const page = parseInt(ctx.query.page || '1', 10);
+	
+	if(page < 1){
+		ctx.status = 400
+		return
+	}
 	try{
-		const posts = await Post.find().sort({_id: -1}).limit(10).exec();//sort({_id: -1}) <- 역순(-1이면 내림차순) .limit(10) 보이는 개수 제한
+		const posts = await Post.find().sort({_id: -1}).limit(10).skip((page-1) * 10).exec();//sort({_id: -1}) <- 역순(-1이면 내림차순) .limit(10) 보이는 개수 제한
 		ctx.body = posts;
 	}catch(e){
 		ctx.throw(500, e);
