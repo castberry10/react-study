f6c2c0c19d73040ea341714626dcfa3c305c6880
최원영 <sable@kangwon.ac.kr>
2023-10-07 14:06:10 +0000

    register 구현

--- blog/blog-backend/src/api/auth/auth.ctrl.js
+++ blog/blog-backend/src/api/auth/auth.ctrl.js
@@ -10,7 +10,43 @@ password" 'B'
 }
 */
 exports.register = async ctx => {
-	
+	//Body 체크
+	const schema = Joi.object().keys({
+		username: Joi.string()
+		.alphanum()
+		.min(3)
+		.max(20)
+		.required(),
+		password: Joi.string().required(),
+	});
+	const result = schema.validate(ctx.request.body);
+	if (result.error){
+		ctx.status = 400;
+		ctx.body = result.error;
+		return;
+	}
+	// 체크 끝
+	const {username, password} = ctx.request.body;
+	try{
+		//username이 존재하는지 체크
+		const exists = await User.findByUsername(username);
+		if(exists){
+			ctx.status = 409 // Conflict
+			return;
+		}
+		const user = new User({
+			username,
+		});
+		await user.setPassword(password);
+		await user.save();
+		
+		//응답할 데이터에서 해쉬비번 제거
+		const data = user.toJSON();
+		delete data.hashedPassword;
+		ctx.body = data;
+	} catch(e){
+		ctx.throw(500, e);
+	}
 };
 
 exports.login = async ctx => {
