1621e6673eed0b4f9315717edef58d1fb3e3505b
최원영 <sable@kangwon.ac.kr>
2023-10-10 11:38:30 +0000

    Route 지정

--- blog/blog-frontend/src/App.js
+++ blog/blog-frontend/src/App.js
@@ -1,11 +1,33 @@
+import {Route, Routes} from 'react-router-dom';
 
-import './App.css';
+import LoginPage from './pages/LoginPage';
+import PostListPage from './pages/PostListPage';
+import PostPage from './pages/PostPage';
+import RegisterPage from './pages/RegisterPage';
+import WritePage from './pages/WritePage';
 
+/*
+/@:username으로 감싼 것은 아래와 같은 기능을 함
+<Route path="/@:username" element={<PostListPage/>}/>
+<Route path="/@:username/:postId" element={<PostPage/>}/>
+
+<Route path="/@:username">
+		  	<Route index element={<PostListPage/>}/>
+		  	<Route path=":postId" element={<PostPage/>}/>
+		  </Route>
+*/
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Routes>
+		  <Route path="/" element={<PostListPage/>}/>
+		  <Route path="/login" element={<LoginPage/>}/>
+		  <Route path="/register" element={<RegisterPage/>}/>
+		  <Route path="/write" element={<WritePage/>}/>
+		  <Route path="/:username">
+		  	<Route index element={<PostListPage/>}/>
+		  	<Route path=":postId" element={<PostPage/>}/>
+		  </Route>
+	</Routes>
   );
 }
 
