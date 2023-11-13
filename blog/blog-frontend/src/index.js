b2ca736b7a4eb529ffb6a96afa7358d33b4f9b7d
최원영 <sable@kangwon.ac.kr>
2023-11-13 11:02:04 +0000

    로그인 상태 유지 -> 새로고침해도 유지

--- blog/blog-frontend/src/index.js
+++ blog/blog-frontend/src/index.js
@@ -9,10 +9,29 @@ import {composeWithDevTools} from 'redux-devtools-extension';
 import rootReducer, { rootSaga } from './modules';
 import {Provider} from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
+import {tempSetUser, check} from './modules/user';
+
+
+
+function loadUser(){
+	try{
+		const user = localStorage.getItem('user');
+		if (!user){
+			return; //로그인 상태가 아니면 아무것도 안함
+		}
+		
+		store.dispatch(tempSetUser(JSON.parse(user)));
+		store.dispatch(check());
+	}catch(e){
+		console.log('localStorage is not working');
+	}
+}
+
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
 sagaMiddleware.run(rootSaga);
+loadUser();
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
