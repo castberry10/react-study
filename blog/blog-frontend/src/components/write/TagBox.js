1a5f804338700ccb758612f068149f7e49bc05c2
최원영 <sable@kangwon.ac.kr>
2023-11-25 09:08:01 +0000

    TagBoxContainer 생성 및 적용

--- blog/blog-frontend/src/components/write/TagBox.js
+++ blog/blog-frontend/src/components/write/TagBox.js
@@ -1,4 +1,4 @@
-import React, {useState, useCallback} from 'react';
+import React, {useState, useCallback, useEffect} from 'react';
 import styled from 'styled-components';
 import palette from '../../lib/styles/palette';
 
@@ -72,7 +72,7 @@ const TagList = React.memo(({tags, onRemove}) => (
 	</TagListBlock>
 ));
 
-const TagBox = () => {
+const TagBox = ({tags, onChangeTags}) => {
 	const [input, setInput] = useState('');
 	const [localTags, setLocalTags] = useState([]);
 	
@@ -80,15 +80,19 @@ const TagBox = () => {
 		tag => {
 			if(!tag) return; //공백이라면 추가 x
 			if(localTags.includes(tag)) return; //이미 존재하면 추가 안함
-			setLocalTags([...localTags, tag]);
+			const nextTags = [...localTags, tag];
+			setLocalTags(nextTags);
+			onChangeTags(nextTags);
 		},
-		[localTags],
+		[localTags, onChangeTags],
 	);
 	const onRemove = useCallback(
 		tag => {
-			setLocalTags(localTags.filter(t => t !== tag));
+			const nextTags = localTags.filter(t => t !== tag);
+			setLocalTags(nextTags);
+			onChangeTags(nextTags)
 		},
-		[localTags],
+		[localTags, onChangeTags],
 	);
 	
 	const onChange = useCallback(e => {
@@ -103,6 +107,10 @@ const TagBox = () => {
 		},
 		[input, insertTag],
 	);
+	//tags 값이 바뀌면 
+	useEffect(()=>{
+		setLocalTags(tags);
+	}, [tags])
 	
 	return(
 		<TagBoxBlock>
--- blog/blog-frontend/src/pages/WritePage.js
+++ blog/blog-frontend/src/pages/WritePage.js
@@ -1,5 +1,5 @@
 
-import TagBox from '../components/write/TagBox';
+import TagBoxContainer from '../containers/write/TagBoxContainer';
 import WriteActionButtons from '../components/write/WriteActionButtons';
 import Responsive from '../components/common/Responsive';
 import EditorContainer from '../containers/write/EditorContainer';
@@ -8,7 +8,7 @@ const WritePage = () => {
 	return (
 	<Responsive>
 		<EditorContainer />
-		<TagBox />
+		<TagBoxContainer />
 		<WriteActionButtons/>
 	</Responsive>
 	);
