41f9da4f61b5fc41cd93b433d393d1cfd1f5ec34
최원영 <sable@kangwon.ac.kr>
2023-11-25 06:44:41 +0000

    EditorContainer 제작 , 적용

--- blog/blog-frontend/src/modules/write.js
+++ blog/blog-frontend/src/modules/write.js
@@ -1,6 +1,6 @@
 import {createAction, handleActions} from 'redux-actions';
 
-const INITIALIZE = 'write/INITALIZE'; //모든 내용 초기화
+const INITIALIZE = 'write/INITIALIZE'; //모든 내용 초기화
 const CHANGE_FIELD = 'write/CHANGE_FIELD'; //특정 key값 바꾸기 
 
 export const initialize = createAction(INITIALIZE);
@@ -17,7 +17,7 @@ const initialState = {
 
 const write = handleActions(
 	{
-		[INITALIZE]:state = > initialState, //
+		[INITIALIZE]:state => initialState, //
 		[CHANGE_FIELD]:(state, {payload: {key, value}}) => ({
 			...state,
 			[key]: value,
